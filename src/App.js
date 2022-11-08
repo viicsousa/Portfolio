a85baea69e14618efb647d25c645b56e37e1a24a
seu nome <104572313+viicsousa@users.noreply.github.com>
2022-11-07 21:28:12 -0300

    modificado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from '../src/components/Navbar/Navbar';
 import Aboutme from '../src/components/Aboutme/Aboutme'
+import Presentarion from './components/Presentation/Presentation';
 
 function App() {
   return (
       <>
       <Navbar/>
       <Aboutme/>
+      <Presentarion/>
       </>
   
   );
--- src/components/Aboutme/Aboutme.jsx
+++ src/components/Aboutme/Aboutme.jsx
@@ -3,16 +3,19 @@ import './Aboutme.css'
 const Aboutme = () => {
     return (
         <div className='container-about'>
-            <div>
-                <h4>Portfólio</h4>
+            <div className='container'>
+                <h4>PORTFÓLIO</h4>
                 <h2>Victória Vasconcelos</h2>
                 <p>
-                    <span>FULL-STACK DEVELEPOR</span>
+                    <span>FULL-STACK DEVELOPER</span>
                 </p>
+                <div className='container-button'>
+                    <button className='button'>Saiba Mais</button>
+                </div>
             </div>
-            
-
+    
         </div>
+        
     )
 }
 
