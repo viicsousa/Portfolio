714fbfbd28fe7e9672f8a65b3b293ac05fc421c5
Victoria <victoria.vasconcelos@hotmail.com>
2023-04-06 10:18:28 -0300

    feat: :art:

--- src/pages/portfolio/index.page.tsx
+++ src/pages/portfolio/index.page.tsx
@@ -1,9 +1,6 @@
-import { Container} from './styles';
-
 export default function Portfolio () {
   return (
-    <>
-  <Container> Eu amo você </Container>
+  <>
   </>
   )
 }
--- src/pages/portfolio/styles.ts
+++ src/pages/portfolio/styles.ts
@@ -1,9 +1,1 @@
 import styled from "styled-components";
-
-export const Container = styled.div`
-  color: blue;
-  font-size: 32px;
-  background-color: black;
-  text-align: center;
-
-`;
