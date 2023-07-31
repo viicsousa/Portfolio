a4e5dbf08773ca7dfccc600ea423fb8df5a1e871
Victória <victoria.vasconcelos@hotmail.com>
2023-07-31 15:30:16 -0300

    feat:

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -22,7 +22,7 @@ export default function About() {
         <Aboutme>Possuo 1 ano de experiência como desenvolvedora web, utilizando as principais tecnologias do mercado.</Aboutme>
         <ContainerInfo>
         <Sociais/>
-          <a href="https://drive.google.com/file/d/12_d-iZ9J_IZj9owjYyRoZgxi8MRIfkPr/view?usp=sharing" target="_blank"><Button>Download CV</Button></a>
+          <a href="https://drive.google.com/file/d/1C7hRKrMEWFbhCDs7qGyq5LDwMTmWv-3M/view?usp=sharing" target="_blank"><Button>Download CV</Button></a>
           </ContainerInfo>
       </TextAbout>
       <ImgAbout>
